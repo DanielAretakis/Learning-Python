32a55fb508ee86d9f71c4fcab6405a04f3f75333
DanielAretakis <daniel.aretakis@hotmail.com>
2021-11-18 20:03:44 -0300

    Ex008

---
+++ Arithmetic Operators - Times table.py
@@ -0,0 +1,5 @@
+nmb = int(input('Type a whole number to see its times table: '))
+print(
+    ' {}x1= {} \n {}x2= {} \n {}x3= {} \n {}x4= {} \n {}x5= {} \n {}x6= {} \n {}x7= {} \n {}x8= {} \n {}x9= {} \n {}x10= {}'.format(
+        nmb, nmb, nmb, nmb * 2, nmb, nmb * 3, nmb, nmb * 4, nmb, nmb * 5, nmb, nmb * 6, nmb, nmb * 7, nmb, nmb * 8, nmb,
+        nmb * 9, nmb, nmb * 10))
