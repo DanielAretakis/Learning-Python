46437125128ff397922cb31f60ff74a51d813e2f
DanielAretakis <daniel.aretakis@hotmail.com>
2021-11-18 18:38:10 -0300

    Ex006

---
+++ Arithmetic Operators - Sum and Div.py
@@ -0,0 +1,4 @@
+g1 = float(input('Type the first grade of the student: '))
+g2 = float(input('Type the second grade of the student: '))
+A = (g1+g2)/2
+print('The average grade of this student is: {}'.format(A))
