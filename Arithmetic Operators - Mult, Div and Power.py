25fcd3c3a2a206688d885e66588d86ea07de2b4b
DanielAretakis <daniel.aretakis@hotmail.com>
2021-11-18 18:28:32 -0300

    Ex005

---
+++ Arithmetic Operators - Mult, Div and Power.py
@@ -0,0 +1,2 @@
+n1 = float(input('Type a number to start the program: '))
+print(' The double of this number is = {} \n The triple of this number is = {} \n The square root of this number is = {}'.format(n1*2, n1*3, n1**1/2))
