3276e2ccd67b3c42ddc5677f8f26cb321c001e10
DanielAretakis <daniel.aretakis@hotmail.com>
2021-11-22 15:51:35 -0300

    Ex010

---
+++ Amount of paint.py
@@ -0,0 +1,4 @@
+wallH = float(input('Type the height of the wall in metres: '))
+wallL = float(input('Type the length of the wall in metres: '))
+paint = wallH*wallL/2
+print("You're going to need a total of {} litres of paint to that wall")
