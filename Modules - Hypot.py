9af345c59275a3a9790f63ea9cfe396bce0d9563
DanielAretakis <daniel.aretakis@hotmail.com>
2021-12-06 16:37:25 -0300

    Ex012

---
+++ Modules - Hypot.py
@@ -0,0 +1,5 @@
+import math
+s1 = int(input('Type the first side of the right triangle to find the hypotenuse: '))
+s2 = int(input('Type the second side: '))
+h = math.hypot(s1, s2)
+print('The hypotenuse of this triangle is {:.2f}cm'.format(h))
