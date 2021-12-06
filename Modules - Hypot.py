1e905ad26ace4ab4ef9b8d886bdf6bd1b7437cf2
DanielAretakis <daniel.aretakis@hotmail.com>
2021-12-06 16:41:50 -0300

    Changed int to float - Ex012

--- Modules - Hypot.py
+++ Modules - Hypot.py
@@ -1,5 +1,5 @@
 import math
-s1 = int(input('Type the first side of the right triangle to find the hypotenuse: '))
-s2 = int(input('Type the second side: '))
+s1 = float(input('Type the first side of the right triangle to find the hypotenuse: '))
+s2 = float(input('Type the second side: '))
 h = math.hypot(s1, s2)
 print('The hypotenuse of this triangle is {:.2f}cm'.format(h))
