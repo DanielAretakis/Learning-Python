29aad54b98a1872e97e44ab1cd97bb7aa22d8e50
DanielAretakis <daniel.aretakis@hotmail.com>
2021-12-17 17:03:02 -0300

    Ex014

---
+++ Modules - Playing a music file.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('musicfile.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+#Apparently I did everything right, just the file has some problem
