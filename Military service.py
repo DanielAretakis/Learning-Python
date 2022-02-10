de0b79b01b1df4d8abda6ca000ea3a894668744b
DanielAretakis <daniel.aretakis@hotmail.com>
2022-02-10 15:46:15 -0300

    Ex023 - Age calculator*

--- Military service.py
+++ Military service.py
@@ -4,8 +4,9 @@ age = int(input('What is your year of birth? '))
 n = date.today()
 ay = n.year
 y = (ay - age - 18) * -1
+y2 = ay - age - 18
 if ay - age > 18:
-    print('You already passed the time to start the military service!')
+    print(f'You already passed the time to start the military service for {y2} year(s)!')
 elif ay - age == 18:
     print('You have to start the military service this year!!')
 elif ay - age < 18:
