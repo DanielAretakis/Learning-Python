de1fe10c548b39a0a7471b47ca3d45cae4f04794
DanielAretakis <daniel.aretakis@hotmail.com>
2022-02-16 18:32:59 -0300

    Ex027 - TimesTable using For

--- TimesTableV2.py
+++ TimesTableV2.py
@@ -1,5 +1,3 @@
-t = 0
 n = int(input('Type a whole number to see its times table: '))
-for c in range(0, 10):
-    print(f'|{n} x {t+1} = {n*(t+1)}|')
-    t += 1
+for c in range(1, 11):
+    print(f'|{n} x {c :2} = {n*(c) :2}|')
