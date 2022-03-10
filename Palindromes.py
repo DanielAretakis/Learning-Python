ab45ae2dae886f0a875179c6b5f56afc814b855c
DanielAretakis <daniel.aretakis@hotmail.com>
2022-03-10 19:56:00 -0300

    Ex028 - Inverting strings and testing palindromes

---
+++ Palindromes.py
@@ -0,0 +1,10 @@
+phrase = input('Type a phrase: ').strip().upper().split()
+at = ''.join(phrase)
+iv = ''
+for l in range(len(at) -1, -1, - 1):
+    iv+=at[l]
+print(f'The inverse of {at} is {iv}')
+if at == iv:
+    print('This phrase is a palindrome')
+else:
+    print('This phrase is not a palindrome')
