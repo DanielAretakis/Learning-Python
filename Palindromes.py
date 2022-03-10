ea69317c95533fbe5ce361de974e67e2ff4d021a
DanielAretakis <daniel.aretakis@hotmail.com>
2022-03-10 20:00:26 -0300

    Ex028 - With and without using 'for'

--- Palindromes.py
+++ Palindromes.py
@@ -2,8 +2,8 @@ phrase = input('Type a phrase: ').strip().upper().split()
 at = ''.join(phrase)
 iv = at[::-1]
 '''iv = ''
-for l in range(len(at) -1, -1, - 1):
-    iv+=at[l]'''
+for lg in range(len(at) -1, -1, - 1):
+    iv+=at[lg]'''
 print(f'The inverse of {at} is {iv}')
 if at == iv:
     print('This phrase is a palindrome')
