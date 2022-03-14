f6406c99f262f1ac9e7bb836da2bbd7c40b1c5e0
DanielAretakis <daniel.aretakis@hotmail.com>
2022-03-14 17:00:09 -0300

    Ex030 - Try to guess the number

---
+++ Guess game.py
@@ -0,0 +1,13 @@
+from random import randint
+
+number = randint(1, 10)
+tries = 0
+c = 1
+while tries != number:
+    tries = int(input('Try to guess the number I am thinking...: '))
+    if tries != number:
+      c += 1
+if c != 1:
+    print(f"Congratulations!! But you needed {c} tries, why don't you try to guess faster this time?")
+else:
+    print("Nice job, you made it on the first try, congratulations!!")
