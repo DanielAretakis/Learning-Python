e83e80448805de187d2f6d6d4d375667566373f7
DanielAretakis <daniel.aretakis@hotmail.com>
2022-03-15 20:22:55 -0300

    Ex030 - Added a tip to help find the number faster

--- Guess game.py
+++ Guess game.py
@@ -6,7 +6,11 @@ c = 1
 while tries != number:
     tries = int(input('Try to guess the number I am thinking...: '))
     if tries != number:
-      c += 1
+        c += 1
+        if tries > number:
+            print('Try a lower number!')
+        if tries < number:
+            print('Try a higher number!')
 if c != 1:
     print(f"Congratulations!! But you needed {c} tries, why don't you try to guess faster this time?")
 else:
